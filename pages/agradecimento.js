066dffedb08314d77cf7ea3a1f1bc9ad391a62cf
brenno sena <brennounasp@gmail.com>
2024-04-28 01:39:05 +0000

    fim do projeto

--- pages/agradecimento.js
+++ pages/agradecimento.js
@@ -1,3 +1,13 @@
 function pegarNota() {
-    let nota = localStorage.getItem("nota")
+    let nota = localStorage.getItem("nota");
+
+    let paragrafoNota = document.querySelector(".paragrafo_nota");
+
+    if (nota !== null) {
+        paragrafoNota.innerHTML = `Você selecionou ${nota} de 5`;
+    } else {
+        paragrafoNota.innerHTML = "Nota não encontrada";
+    }
 }
+
+pegarNota();
